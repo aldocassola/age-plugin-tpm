b6ea98ade92a600bca8c12e7bbbec51615103fe7
Aldo Cassola <aldocassola@gmail.com>
2024-11-24 13:31:33 -0500

    abstract out tpm open

--- cmd/age-plugin-tpm/main.go
+++ cmd/age-plugin-tpm/main.go
@@ -10,7 +10,7 @@ import (
 
 	"filippo.io/age"
 	page "filippo.io/age/plugin"
-	"github.com/foxboron/age-plugin-tpm/plugin"
+	"github.com/aldocassola/age-plugin-tpm/plugin"
 	"github.com/google/go-tpm/tpm2/transport"
 	"github.com/spf13/cobra"
 	"golang.org/x/term"
@@ -288,7 +288,7 @@ func RunPlugin(cmd *cobra.Command, args []string) error {
 	return nil
 }
 
-func pluginFlags(cmd *cobra.Command, opts *PluginOptions) {
+func pluginFlags(cmd *cobra.Command, _ *PluginOptions) {
 	flags := cmd.Flags()
 	flags.SortFlags = false
 
